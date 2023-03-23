cb87ed264685b59ef784a6b32429ea4f96d6d30f
Greattie <gamubode@gmail.com>
2023-03-23 09:59:55 +0100

    finished adding javascript

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let btns = document.querySelectorAll('.btn')
 let count = 0
 const value = document.getElementById('value')
 
+btns.forEach(function(btn){
+btn.addEventListener('click', function(e){
+const classNames = e.currentTarget.classList
+if(classNames.contains('minus')){
+    count--
+    value.textContent = count;
+}
+else if(classNames.contains('plus')){
+    count ++
+    value.textContent = count
+}
+else {
+    count = 0
+}
+value.textContent = count
+})
+})
