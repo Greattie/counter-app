88e88e760684f18e2ec10b17eb65afa0be7c54c9
Greattie <gamubode@gmail.com>
2023-03-24 09:47:45 +0100

    fixed bug in the count style

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let btns = document.querySelectorAll('.btn')
-let count = 0
-const value = document.getElementById('value')
+let btns = document.querySelectorAll(".btn");
+let count = 0;
+const value = document.getElementById("value");
 
-btns.forEach(function(btn){
-btn.addEventListener('click', function(e){
-const classNames = e.currentTarget.classList
-if(classNames.contains('minus')){
-    count--
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const classNames = e.currentTarget.classList;
+    if (classNames.contains("minus")) {
+      count--;
+      value.textContent = count;
+    } else if (classNames.contains("plus")) {
+      count++;
+      value.textContent = count;
+      console.log(value.textContent);
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
     value.textContent = count;
-}
-else if(classNames.contains('plus')){
-    count ++
-    value.textContent = count
-}
-else {
-    count = 0
-}
-
-if(count>0){
-    value.style.color = 'green'
-}
-if(count < 0){
-    value.style.color = 'red'
-}else if(count = 0){
-    value.style.color = 'black'
-}
-value.textContent = count
-})
-})
+  });
+});
